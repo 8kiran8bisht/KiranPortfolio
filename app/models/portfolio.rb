ab127c676e66e247590b4ff8824ff42119da6c43
8kiran8bisht <8kiran8bisht@gmail.com>
2020-07-23 05:01:21 -0400

    defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,11 @@ class Portfolio < ApplicationRecord
 	# 	where(subtitle: 'Angular')
 	# end
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Angular')} 
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/290C/O%20https://placeholder.com/"
+		self.thumb_image ||= "https://via.placeholder.com/190C/O%20https://placeholder.com/"
+	end
 end
